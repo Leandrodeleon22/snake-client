feea2356895f60bc8446885c078f8d33f3af236d
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-15 16:20:06 -0500

    feat(client): add move commands

--- client.js
+++ client.js
@@ -10,8 +10,15 @@ const connect = function () {
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     conn.write("Name: LLL");
+
     console.log("Successfully connected to game server");
   });
+  conn.on("connect", () => {
+    setTimeout(() => {
+      conn.write("Move: up");
+    }, 1000);
+  });
+
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
