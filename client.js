e3d2e9c9b6f39ffd72796f9f20e02e9a2ce7d9e5
leandrodeleon22 <leandrodeleon22@gmail.com>
2023-11-15 19:10:37 -0500

    refactor: remove  comments

--- client.js
+++ client.js
@@ -7,18 +7,12 @@ const connect = function () {
     port: PORT,
   });
 
-  //interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     conn.write("Name: LLL");
 
     console.log("Successfully connected to game server");
   });
-  // conn.on("connect", () => {
-  //   setTimeout(() => {
-  //     conn.write("Move: up");
-  //   }, 1000);
-  // });
 
   conn.on("data", (data) => {
     console.log("Server says: ", data);
